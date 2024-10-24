b73dd6fd439f811aa0e836ebd422a301aebd74c5
Vitthal Popat Gund <97181033+VitthalGund@users.noreply.github.com>
2024-10-25 01:12:30 +0530

    filter done

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,15 +1,14 @@
 import { BannerCarousel } from "@/components/BannerCarousel";
+import FilterComponent from "@/components/Filter";
 
 export default function Home() {
-
   return (
     <>
       <main className="flex flex-col gap-8 row-start-2 items-center max-w-[1000px] justify-center m-auto">
-       <BannerCarousel/>
+        <BannerCarousel />
+        <FilterComponent></FilterComponent>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </>
-    
   );
 }
