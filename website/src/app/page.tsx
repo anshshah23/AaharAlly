3e60f5f06ea480575da7c9be22642cb26134b7c4
Vitthal Popat Gund <97181033+VitthalGund@users.noreply.github.com>
2024-10-25 00:17:25 +0530

    component remained

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,11 +1,11 @@
-import { HomeCarouse } from "@/components/Carousel";
+import { BannerCarousel } from "@/components/BannerCarousel";
 
 export default function Home() {
 
   return (
     <>
       <main className="flex flex-col gap-8 row-start-2 items-center max-w-[1000px] justify-center m-auto">
-       <HomeCarouse/>
+       <BannerCarousel/>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
