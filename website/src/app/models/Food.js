f4846c6ccf1a18a04e83918e158d961269921a4f
Ashish48Maurya <maurya.48.ashish.com>
2024-10-25 23:10:28 +0530

    Food Schema Changed

--- website/src/app/models/Food.js
+++ website/src/app/models/Food.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 const foodSchema = new mongoose.Schema({ //require 20 document for each category of food => total 20*10 = 200
-    item_name:{
-        type:String,
-        required:true,
+    name: {
+        type: String,
+        required: true,
     },
-    item_url:{
-        type:String,
-        required:true,
+    image: {
+        type: String,
+        required: true,
     },
-    item_rating:{
-        type:Number,
-        required:true,
+    rating: {
+        type: String,
+        required: true,
     },
-    item_price:{
-        type:Number,
-        required:true,
+    price: {
+        type: String,
+        required: true,
     },
-    item_desc:{
-        type:String,
-        required:true
-    }
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
 })
-export const food = mongoose.models.food || mongoose.model('food',foodSchema)
+export const food = mongoose.models.food || mongoose.model('food', foodSchema)
