4a508ab1ff19ea96a1583253b0570ca3065ce5f4
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-10-26 11:05:13 +0530

    age modal fixed

--- website/src/components/AgeModal.tsx
+++ website/src/components/AgeModal.tsx
@@ -66,6 +66,7 @@ const AgeModal: React.FC<AgeModalProps> = ({ isModalOpen, setIsModalOpen }) => {
           age,
         },
       })
+      setIsModalOpen(false);
     }
   }
 
--- website/src/components/FooterBar.tsx
+++ website/src/components/FooterBar.tsx
@@ -53,7 +53,7 @@ const FooterBar: React.FC<FooterBarProps> = ({ cartItems, notifications }) => {
   };
 
   return (
-    <div className="fixed bottom-0 flex w-full bg-whitescale-105 justify-center">
+    <div className="fixed bottom-0 flex w-full bg-whitescale-105 justify-center z-50">
       <div className="fixed bottom-0 w-[40vw] flex justify-around items-center rounded-3xl px-4 mx-2 my-4 z-50 bg-orangeCustom text-white shadow-md shadow-orangeCustom transition duration-500">
         {buttons.map((button, index) => (
           <div
